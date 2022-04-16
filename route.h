9904ec0f7f27a9dcb7bb933d824908886744de84
Airam Rafael Luque León <alu0101335148@ull.edu.es>
2022-04-17 00:22:42 +0100

    Agregada documentación

--- route.h
+++ route.h
@@ -1,3 +1,14 @@
+/**
+ * @file route.h
+ * @author Airam Rafael Luque León (alu0101335148@ull.edu.es)
+ * @brief This file contains the definition of the class Route.
+ * @version 1.2
+ * @date 2022-04-14
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
 #ifndef ___ROUTE___
 #define ___ROUTE___
 
@@ -5,35 +16,69 @@
 #include <string>
 #include <vector>
 
+/**
+ * @brief This class implement a basic route.
+ * @details This class have a vector of nodes, that represents the route and
+ * the cost of the route.
+ */
 class Route {
   private:
     std::vector<int> route_ = {};
     int cost_ = 0;
 
   public:
+    /**
+     * @brief Constructor of the class
+     */
     Route() {};
+    /**
+     * @brief Destructor of the class
+     */
     ~Route() {};
 
+    /**
+     * @brief Get the Size object
+     * @return int size
+     */
     int getSize() {
       return route_.size();
     }
 
+    /**
+     * @brief Get the Route object
+     * @return std::vector<int> route
+     */
     std::vector<int> getRoute() {
       return route_;
     }
 
+    /**
+     * @brief Get the Cost object
+     * @return int& cost of the route
+     */
     int& getCost() {
       return cost_;
     }
 
+    /**
+     * @brief Get the last node of the route
+     * @return int las node
+     */
     int getLastClient() {
       return route_.back();
     }
 
+    /**
+     * @brief This method adds a new node to the route
+     * @param client node to add
+     */
     void addClient(int client) {
       route_.push_back(client);
     }
 
+    /**
+     * @brief This method prints the route and the cost of the route
+     */
     void printRoute() {
       std::string result = "[ ";
       for (int i = 0; i < route_.size(); i++) {
@@ -44,7 +89,6 @@ class Route {
       }
       std::cout << result << " ]\tCost: " << cost_ << "\n";
     }
-
 };
 
 #endif
